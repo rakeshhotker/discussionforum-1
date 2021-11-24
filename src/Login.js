696826acac6229b55e6da3ddd5626ece92a9a0c0
AbhinavCS2702 <1602-19-737-123@vce.ac.in>
2021-11-24 16:01:23 +0530

    created login files

---
+++ src/Login.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <div class="left">
+                <div id="heading">
+                    <h3>Vasavi Connect</h3>
+                    <p>A place to meet like minded peers...</p>
+                </div>
+                <div id="greeting">
+                    <p>Welcome back! Please login to your account</p>
+                </div>
+                <div>
+                    <h5>Login</h5>
+                    <div>
+                        <div>
+
+                        </div>
+                        <div id="email_field" class="credentials">
+                            <label>Email Address</label>
+                            <input placeholder= "enter email address" type="email"/>
+                        </div>
+                        <div id="email_field" class="credentials">
+                            <label>Password</label>
+                            <input placeholder= "enter password" type= "password"/>
+                        </div>
+                        <div>
+                            <p><a href="#">Forgot Password?</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="buttons">
+                    <div id="Login">
+                        <button>Login</button>
+
+                    </div>
+                    <div id="Sign Up">
+                        <button>Sign Up</button>
+
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+
+
+            </div>
+        
+        </div>
+    )
+}
+
+export default Login
